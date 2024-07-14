a235bb82c810b651a1e14641f17fd5dca9db9cc8
Kage <romzhey@gmail.com>
2024-07-14 23:03:02 +0800

    added tsconfigPaths plugin

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest"/>
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: "jsdom",
